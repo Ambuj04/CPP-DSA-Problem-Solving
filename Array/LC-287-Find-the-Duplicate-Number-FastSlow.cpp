f65200348fcc12a1e78448614c06fea08b827a1b
ambuj <ambujsdl2004@gmail.com>
2026-02-18 20:51:56 +0530

    Find duplicate number (fast slow pointer) added

---
+++ Array/LC-287-Find-the-Duplicate-Number-FastSlow.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+  int findDuplicate(vector<int> &nums)
+  {
+    int slow = 0;
+    int fast = 0;
+    while (true)
+    {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+      if (fast == slow) 
+      {
+        slow = 0;
+        while (fast != slow) 
+        {
+          slow = nums[slow];
+          fast = nums[fast];
+        }
+        return slow;
+      }
+    }
+    return -1;
+  }
+};
